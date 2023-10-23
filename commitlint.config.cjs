fcc5add3945ce5bf40e02ec71c24c2d18b49c3d7
mnauff <mnaufalrafianto@gmail.com>
2023-10-23 15:21:28 +0700

    feat: initial commit

---
+++ commitlint.config.cjs
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'ci', 'test', 'revert', 'perf', 'vercel'],
+        ],
+    },
+}
